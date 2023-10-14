7bdb9ebce50a272e7d4df785eee4d251371609d5
H <62352013+iHector1@users.noreply.github.com>
2023-10-14 13:23:59 -0600

    smoke esta hecho

--- library/src/FloodFill.java
+++ library/src/FloodFill.java
@@ -1,16 +1,154 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+public class FloodFill extends JFrame {
+    private BufferedImage buffer;
+    private int width;
+    private int height;
 
-public class FloodFill {
-    private Location location;
+    public FloodFill(int width, int height) {
+        this.width = width;
+        this.height = height;
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
-    FloodFill(Location location){
-        this.location = location;
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(width, height);
+        setTitle("Flood Fill Example");
     }
 
-    public ArrayList<Location> ScanLine(){
-        ArrayList<Location> result = new ArrayList<>();
+    private void fillFigure(int[][] points) {
+        int n = points.length;
+        int startX = Integer.MAX_VALUE;
+        int startY = Integer.MAX_VALUE;
+        int endX = Integer.MIN_VALUE;
+        int endY = Integer.MIN_VALUE;
 
-        return result;
+        // Find the bounding box of the rotated figure
+        for (int i = 0; i < n; i++) {
+            int x = points[i][0];
+            int y = points[i][1];
+            startX = Math.min(startX, x);
+            startY = Math.min(startY, y);
+            endX = Math.max(endX, x);
+            endY = Math.max(endY, y);
+        }
+
+        int direction = 0; // 0: right, 1: down, 2: left, 3: up
+        int x = startX;
+        int y = startY;
+
+        while (startX <= endX && startY <= endY) {
+            // Paint pixels in the current direction
+            while (x >= startX && x <= endX && y >= startY && y <= endY) {
+                if (isInsidePolygon(x, y, points)) {
+                    putPixel(x, y, Color.red);
+                }
+                // Move to the next pixel in the current direction
+                switch (direction) {
+                    case 0:
+                        x++;
+                        break;
+                    case 1:
+                        y++;
+                        break;
+                    case 2:
+                        x--;
+                        break;
+                    case 3:
+                        y--;
+                        break;
+                }
+            }
+
+            // Update the bounding box based on the current direction
+            switch (direction) {
+                case 0:
+                    startY++;
+                    x--;
+                    y++;
+                    break;
+                case 1:
+                    endX--;
+                    x--;
+                    y--;
+                    break;
+                case 2:
+                    endY--;
+                    x++;
+                    y--;
+                    break;
+                case 3:
+                    startX++;
+                    x++;
+                    y++;
+                    break;
+            }
+
+            // Change direction (right -> down -> left -> up)
+            direction = (direction + 1) % 4;
+        }
     }
 
+    private boolean isInsidePolygon(int x, int y, int[][] points) {
+        int n = points.length;
+        boolean isInside = false;
+
+        for (int i = 0, j = n - 1; i < n; j = i++) {
+            int xi = points[i][0];
+            int yi = points[i][1];
+            int xj = points[j][0];
+            int yj = points[j][1];
+
+            if (((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)) {
+                isInside = !isInside;
+            }
+        }
+
+        return isInside;
+    }
+
+    private void putPixel(int x, int y, Color c) {
+        buffer.setRGB(x, y, c.getRGB());
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(buffer, 0, 0, this);
+    }
+
+    public static void main(String[] args) {
+        int width = 500;
+        int height = 500;
+
+        FloodFill frame = new FloodFill(width, height);
+
+        // Dibujar un cuadrado
+        int[][] squarePoints = {
+                {100, 100},  // Top-left
+                {200, 100},  // Top-right
+                {200, 200},  // Bottom-right
+                {100, 200}   // Bottom-left
+        };
+        frame.fillFigure(squarePoints);
+
+        // Dibujar un círculo
+        int centerX = 250;
+        int centerY = 250;
+        int radius = 50;
+
+        int numPoints = 360;  // Número de puntos para aproximar el círculo
+        int[][] circlePoints = new int[numPoints][2];
+
+        for (int i = 0; i < numPoints; i++) {
+            double angle = 2 * Math.PI * i / numPoints;
+            int x = (int) (centerX + radius * Math.cos(angle));
+            int y = (int) (centerY + radius * Math.sin(angle));
+            circlePoints[i] = new int[]{x, y};
+        }
+        frame.fillFigure(circlePoints);
+
+        frame.setVisible(true);
+    }
 }
---
+++ library/src/Smoke.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class Smoke extends JFrame {
+
+    private Color color;
+    private BufferedImage buffer;
+    private Graphics graphics;
+    private ArrayList<Location> locations;
+    private Figures g;
+    private int points;
+    public Smoke(int points){
+        this.g = new Figures();
+        color = Color.BLUE;
+        setTitle("Smoke "+points);
+        setSize(900, 450);
+        setLayout(null);
+        setVisible(true);
+        this.points = points;
+        buffer = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+        graphics = (Graphics2D) buffer.createGraphics();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    @Override
+    public void paint(Graphics graphics){
+        super.paint(graphics);
+        this.drawSmoke();
+    }
+    private void drawSmoke()
+    {
+        ArrayList<Location> pointsXY = new ArrayList<>();
+        int x;
+        for (int y = 0; y > -360; y--)
+        {
+            double radian = Math.PI/180 *(y*4);
+            x = (int) (Math.cos(radian) * -y);
+            putPixel(x + 400, y + 400);
+            pointsXY.add(new Location(x + 400, y + 400));
+        }
+        drawLines(pointsXY);
+    }
+    private void putPixel(int x,int y){
+        buffer.setRGB(0, 0, color.getRGB());
+        this.getGraphics().drawImage(buffer, x, y, this);
+    }
+
+    public static void main(String[] args) {
+        new Smoke(100);
+    }
+    private void drawLines(ArrayList<Location> points)
+    {
+        for (int i = 0; i < points.size()-1; i++)
+        {
+            ArrayList<Location> locations = g.bresenham(points.get(i),points.get(i+1));
+            drawPoints(locations);
+        }
+    }
+    public void drawPoints(ArrayList<Location> locations){
+        for (Location point : locations){
+            System.out.println((point.pointX)+" , "+(point.pointY));
+            putPixel(point.pointX, point.pointY);
+        }
+    }
+}
