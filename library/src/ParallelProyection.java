e3b6427edf591310c482fb7043fee52f43e61aef
H <62352013+iHector1@users.noreply.github.com>
2023-11-06 19:28:44 -0600

    proyeccion paralela hecha

--- library/src/ParallelProyection.java
+++ library/src/ParallelProyection.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
-public class ObliqueParallel extends JFrame {
+public class ParallelProyection extends JFrame {
     private Color color;
     private BufferedImage buffer;
     private Graphics graphics;
@@ -12,26 +12,26 @@ public class ObliqueParallel extends JFrame {
     private ArrayList<Location3D> pointsXYZ = new ArrayList<Location3D>();
     private Location3D vector;
     private Figures g;
-    public ObliqueParallel(){
-        color = Color.BLUE;
+    public ParallelProyection(){
+        color = Color.red;
         setTitle("Proyección paralela oblicua");
         setSize(450, 450);
         setLayout(null);
         setVisible(true);
 
         this.g = new Figures();
+        //first square
+        pointsXYZ.add(new Location3D(50, 150, 150));
+        pointsXYZ.add(new Location3D(150, 150, 150));
+        pointsXYZ.add(new Location3D(50, 250, 150));
+        pointsXYZ.add(new Location3D(150, 250, 150));
+        //second square
+        pointsXYZ.add(new Location3D(50, 150, 250));
+        pointsXYZ.add(new Location3D(150, 150, 250));
+        pointsXYZ.add(new Location3D(50, 250, 250));
+        pointsXYZ.add(new Location3D(150, 250, 250));
 
-        pointsXYZ.add(new Location3D(50, 150, 150)); // A
-        pointsXYZ.add(new Location3D(150, 150, 150)); // B
-        pointsXYZ.add(new Location3D(50, 250, 150)); // C
-        pointsXYZ.add(new Location3D(150, 250, 150)); // D
-
-        pointsXYZ.add(new Location3D(50, 150, 250)); // E
-        pointsXYZ.add(new Location3D(150, 150, 250)); // F
-        pointsXYZ.add(new Location3D(50, 250, 250)); // G
-        pointsXYZ.add(new Location3D(150, 250, 250)); // H
-
-        vector = new Location3D(-4,-5,7);
+        vector = new Location3D(8,7,20);
 
         buffer = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
         graphics = (Graphics2D) buffer.createGraphics();
@@ -43,7 +43,7 @@ public class ObliqueParallel extends JFrame {
 
     private void drawCube(){
         for (int i = 0; i < pointsXYZ.size(); i++){
-            float u =  (float) (-1 * pointsXYZ.get(i).pointZ) / vector.pointZ;
+            float u =  (float) (pointsXYZ.get(i).pointZ) / vector.pointZ;
             float x = pointsXYZ.get(i).pointX + (vector.pointX * u);
             float y = pointsXYZ.get(i).pointY + (vector.pointY * u);
 
@@ -51,7 +51,6 @@ public class ObliqueParallel extends JFrame {
         }
         for (Location point: pointsXY){
             putPixel(point.pointX, point.pointY);
-            System.out.println("x:" + point.pointX + " y:" + point.pointY);
         }
 
         ArrayList<Location> pointsAB = g.bresenham(pointsXY.get(0), pointsXY.get(1));
@@ -98,6 +97,6 @@ public class ObliqueParallel extends JFrame {
     }
 
     public static void main(String[] args) {
-        new ObliqueParallel();
+        new ParallelProyection();
     }
 }
